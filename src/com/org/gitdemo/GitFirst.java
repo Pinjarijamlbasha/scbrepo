27a198f3519b9f0912dd0a44b9594d510206795b
Pinjarijamlbasha <126856228+Pinjarijamlbasha@users.noreply.github.com>
2024-05-22 21:58:31 +0530

    Second commit GitFirst.java

--- src/com/org/gitdemo/GitFirst.java
+++ src/com/org/gitdemo/GitFirst.java
@@ -3,7 +3,7 @@ package com.org.gitdemo;
 public class GitFirst {
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome to Jamal Basha");
+		System.out.println("Welcome to Jamal Basha github ki ra mava");
 	}
 
 }
